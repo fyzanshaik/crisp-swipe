81ea61d486d45483a232d524c4ce6db289165a73
fyzanshaik <fyzan.shaik@gmail.com>
2025-09-27 17:16:44 +0530

    serving frontend from build through server
    Signed-off-by: fyzanshaik <fyzan.shaik@gmail.com>

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,6 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
-
+import { serveStatic } from "hono/bun";
 const app = new Hono();
 
 app.use("*", logger());
@@ -8,5 +8,8 @@ app.use("*", logger());
 app.get("/hello-world", (c) => {
   return c.json({ message: "hello world" });
 });
+
+app.use("*", serveStatic({ root: "./frontend/dist" }));
+app.use("*", serveStatic({ path: "./frontend/dist/index.html" }));
 
 export default app;
