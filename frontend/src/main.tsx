b81c8fa034c7ac75bc9016a8248b1dcb1bffac28
fyzanshaik <fyzan.shaik@gmail.com>
2025-09-27 21:17:42 +0530

    add tanstack router form query kinde auth
    Signed-off-by: fyzanshaik <fyzan.shaik@gmail.com>

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,28 @@
+import { scan } from "react-scan";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "@/components/ui/theme-provider";
-import App from "./App.tsx";
 import "./index.css";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+scan({
+  enabled: true,
+});
 
 const queryClient = new QueryClient();
+const router = createRouter({ routeTree });
 
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <App />
+        <RouterProvider router={router} />
       </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>,
